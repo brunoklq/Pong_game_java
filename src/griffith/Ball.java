47bcba9116005d3395cc3a6f82682af700abf73c
Your NameAndrei <serianu.andrei@gmail.com>
2025-04-01 21:59:50 +0100

    Creating the Constructor

--- src/griffith/Ball.java
+++ src/griffith/Ball.java
@@ -10,4 +10,21 @@ public class Ball {
 	int yVelocity;
 	int initialSpeed = 2;
 
+	Ball(int x, int y, int width, int height){
+		super(x,y,width,height);
+		random = new Random();
+		int randomXDirection = random.nextInt(2);
+		if(randomXDirection == 0)
+			randomXDirection--;
+		setXDirection(randomXDirection*initialSpeed);
+		
+		int randomYDirection = random.nextInt(2);
+		if(randomYDirection == 0)
+			randomYDirection--;
+		setYDirection(randomYDirection*initialSpeed);
+		
+		
+	}
+	
 }
+
