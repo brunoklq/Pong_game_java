696e929d29c1b8bc3e138b37eeecb4e251b0426e
Amadrid <antonioandres.madridbroce@student.griffith.ie>
2025-04-02 11:48:04 +0100

    add game with and height final values

--- src/griffith/GamePanel.java
+++ src/griffith/GamePanel.java
@@ -7,6 +7,12 @@ import javax.swing.*;
 
 
 public abstract class GamePanel extends JPanel implements Runnable{
+	
+	static final int GAME_WIDTH = 1000;
+
+	static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
+
+
 }
 
 
