cd08d33a0758798ff0d2a005ea7111c11eac132f
Amadrid <antonioandres.madridbroce@student.griffith.ie>
2025-04-02 11:51:10 +0100

    added screensize final value

--- src/griffith/GamePanel.java
+++ src/griffith/GamePanel.java
@@ -11,6 +11,8 @@ public abstract class GamePanel extends JPanel implements Runnable{
 	static final int GAME_WIDTH = 1000;
 
 	static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
+	
+	static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH,GAME_HEIGHT);
 
 
 }
