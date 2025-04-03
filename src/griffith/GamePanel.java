25570207e0ee8533b23fcc4fb785918adea1f094
Bruno Kennedy <bruno.klqueiroz@gmail.com>
2025-04-03 19:02:22 +0100

    fixing the position of attributes from GamePanel

--- src/griffith/GamePanel.java
+++ src/griffith/GamePanel.java
@@ -30,10 +30,13 @@ public abstract class GamePanel extends JPanel implements Runnable {
 	
 	Paddle paddle1;
 	Paddle paddle2;
+	Ball ball;
+	Score score;
 
 
 			GamePanel(){
 			
+				newPaddles();
 				
 			
 				this.setPreferredSize(SCREEN_SIZE);
@@ -67,7 +70,7 @@ public abstract class GamePanel extends JPanel implements Runnable {
 			public void move() {
 				paddle1.move();
 				paddle2.move();
-				ball.move();
+				//ball.move();
 			}
 		    public class AL extends KeyAdapter {
 		        public void keyPressed(KeyEvent e) {
--- src/griffith/PongGame.java
+++ src/griffith/PongGame.java
@@ -6,7 +6,7 @@ public class PongGame {
 	public static void main(String[] args) {
 		
 		
-		GameFrame frame = new GrameFrame();
+		GameFrame frame = new GameFrame();
 		
 	}
 }
