0eedc515cd18960ed654eb2e395200c792ae6ebd
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-15 20:19:15 +0100

    Added Paddle Test Class

---
+++ src/griffith/PaddleTest.java
@@ -0,0 +1,10 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.awt.*;
+
+public class PaddleTest {
+
+}
