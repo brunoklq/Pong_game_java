c8e4cfc042e5c7e46dab7125e768cebdd55a2ae7
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-15 20:22:25 +0100

    Add resetPosition() method to Paddle class

--- src/griffith/Paddle.java
+++ src/griffith/Paddle.java
@@ -58,7 +58,7 @@ public class Paddle extends Rectangle {
     public void setYDirection(int yDirection) {
         yVelocity = yDirection;
     }
-
+    
     public void move() {
         y += yVelocity;
     }
--- src/griffith/PaddleTest.java
+++ src/griffith/PaddleTest.java
@@ -3,8 +3,18 @@ package griffith;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.awt.*;
 
-public class PaddleTest {
+class PaddleTest {
 
+    Paddle paddle;
+
+    @BeforeEach
+    void setUp() {
+        paddle = new Paddle(0, 200, 25, 100, 1); // x, y, width, height, id
+    }
+
+    @Test
+    void testGetCenterY() {
+        assertEquals(250, paddle.getCenterY()); // 200 + 100/2 = 250
+    }
 }
