b7ff50a463e30d95311be72665925d4ea1b1ab33
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-15 20:36:58 +0100

    Add color attribute and draw paddle with custom color

--- src/griffith/Paddle.java
+++ src/griffith/Paddle.java
@@ -11,6 +11,7 @@ public class Paddle extends Rectangle {
     int id;
     int yVelocity;
     int speed = 10; //Speed of paddle
+    private final int initialY = y;
 
     public Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id) {
         super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
@@ -61,7 +62,21 @@ public class Paddle extends Rectangle {
     
     public void move() {
         y += yVelocity;
+
+        // Keep paddle inside screen vertically
+        if (y < 0) {
+            y = 0;
+        }
+        if (y > GamePanel.GAME_HEIGHT - height) {
+            y = GamePanel.GAME_HEIGHT - height;
+        }
     }
+    
+    //reset the paddle to its original Y position
+    public void resetPosition() {
+        y = initialY;
+    }
+    
     //Draw the paddle on screen
     public void draw(Graphics g) {
         if (id == 1) {
