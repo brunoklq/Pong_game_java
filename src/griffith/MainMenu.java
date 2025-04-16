03df2e8071975213179d21a700333af0fffdce17
Andrei <serianu.andrei@gmail.com>
2025-04-16 11:03:09 +0100

    Implemented the font and the color

--- src/griffith/MainMenu.java
+++ src/griffith/MainMenu.java
@@ -3,10 +3,15 @@ package griffith;
 import java.awt.Font;
 import java.awt.Graphics;
 
+
 public class MainMenu {
 
 	public void render(Graphics g) {
 		Font fnt0 = new Font("monospaced", Font.BOLD, 50);
+		g.setFont(fnt0);
+		g.setColor(Color.white);
+		g.drawString("Pong Pro Max", GamePanel.WIDTH / 2, 100);
+		
 	}
 	
 	
