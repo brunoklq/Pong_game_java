c2031b0f7ff726bcfaf3a395f0ec6c8407519abe
Andrei <serianu.andrei@gmail.com>
2025-04-16 12:46:19 +0100

    Modified the buttons

--- src/griffith/MainMenu.java
+++ src/griffith/MainMenu.java
@@ -7,8 +7,8 @@ import java.awt.Color;
 public class MainMenu {
 
 	public Rectangle playButton = new Rectangle(Game.GAME_WIDTH/2 + 120 ,150 ,100 ,50);
-	public Rectangle playButton = new Rectangle(Game.GAME_WIDTH/2 + 120 ,150 ,100 ,50);
-	public Rectangle playButton = new Rectangle(Game.GAME_WIDTH/2 + 120 ,150 ,100 ,50);
+	public Rectangle helpButton = new Rectangle(Game.GAME_WIDTH/2 + 120 ,150 ,100 ,50);
+	public Rectangle quitButton = new Rectangle(Game.GAME_WIDTH/2 + 120 ,150 ,100 ,50);
 	
 	public void render(Graphics g) {
 		Font fnt0 = new Font("arial", Font.BOLD, 50);
