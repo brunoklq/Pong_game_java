7f65bcd5207907202ddb466a60af15bff59234c8
Andrei <serianu.andrei@gmail.com>
2025-04-16 12:51:05 +0100

    Bug Fix

--- src/griffith/MainMenu.java
+++ src/griffith/MainMenu.java
@@ -3,14 +3,17 @@ package griffith;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Rectangle;
 
 public class MainMenu {
 
-	public Rectangle playButton = new Rectangle(Game.GAME_WIDTH/2 + 120 ,150 ,100 ,50);
-	public Rectangle helpButton = new Rectangle(Game.GAME_WIDTH/2 + 120 ,150 ,100 ,50);
-	public Rectangle quitButton = new Rectangle(Game.GAME_WIDTH/2 + 120 ,150 ,100 ,50);
+	public Rectangle playButton = new Rectangle(GamePanel.GAME_WIDTH / 2 + 120 ,150 ,100 ,50);
+	public Rectangle helpButton = new Rectangle(GamePanel.GAME_WIDTH / 2 + 120 ,250 ,100 ,50);
+	public Rectangle quitButton = new Rectangle(GamePanel.GAME_WIDTH / 2 + 120 ,350 ,100 ,50);
 	
 	public void render(Graphics g) {
+		
+		
 		Font fnt0 = new Font("arial", Font.BOLD, 50);
 		g.setFont(fnt0);
 		g.setColor(Color.white);
