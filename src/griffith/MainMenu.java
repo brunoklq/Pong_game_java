c66f0231b7f623dc8e812b22ad3ea7cc35a3b990
Andrei <serianu.andrei@gmail.com>
2025-04-16 12:43:29 +0100

    Added buttons in the Main Menu

--- src/griffith/MainMenu.java
+++ src/griffith/MainMenu.java
@@ -6,6 +6,10 @@ import java.awt.Color;
 
 public class MainMenu {
 
+	public Rectangle playButton = new Rectangle(Game.GAME_WIDTH/2 + 120 ,150 ,100 ,50);
+	public Rectangle playButton = new Rectangle(Game.GAME_WIDTH/2 + 120 ,150 ,100 ,50);
+	public Rectangle playButton = new Rectangle(Game.GAME_WIDTH/2 + 120 ,150 ,100 ,50);
+	
 	public void render(Graphics g) {
 		Font fnt0 = new Font("arial", Font.BOLD, 50);
 		g.setFont(fnt0);
