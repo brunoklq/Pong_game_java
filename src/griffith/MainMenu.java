db80b3e57a5d91448cf1a375e24739439036ae08
Andrei <serianu.andrei@gmail.com>
2025-04-16 12:58:37 +0100

    Finishing coding for the time beeing

--- src/griffith/MainMenu.java
+++ src/griffith/MainMenu.java
@@ -8,9 +8,9 @@ import java.awt.Rectangle;
 
 public class MainMenu {
 
-	public Rectangle playButton = new Rectangle(GamePanel.GAME_WIDTH / 2 + 120 ,150 ,100 ,50);
-	public Rectangle helpButton = new Rectangle(GamePanel.GAME_WIDTH / 2 + 120 ,250 ,100 ,50);
-	public Rectangle quitButton = new Rectangle(GamePanel.GAME_WIDTH / 2 + 120 ,350 ,100 ,50);
+	public Rectangle playButton = new Rectangle(GamePanel.GAME_WIDTH / 3 + 120 ,150 ,100 ,50);
+	public Rectangle helpButton = new Rectangle(GamePanel.GAME_WIDTH / 3 + 120 ,250 ,100 ,50);
+	public Rectangle quitButton = new Rectangle(GamePanel.GAME_WIDTH / 3 + 120 ,350 ,100 ,50);
 	
 	public void render(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
@@ -20,6 +20,9 @@ public class MainMenu {
 		g.setColor(Color.white);
 		g.drawString("Pong Pro Max", GamePanel.GAME_WIDTH / 3, 100);
 		
+		Font fnt1 = new Font("arial", Font.BOLD, 30);
+		g.setFont(fnt1);
+		g.drawString("Play", playButton.x, playButton.y);
 		g2d.draw(playButton);
 		g2d.draw(helpButton);
 		g2d.draw(quitButton);
