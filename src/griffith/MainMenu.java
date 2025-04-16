376fb6a26d6cc8a49e228e50464063769ee54c34
Andrei <serianu.andrei@gmail.com>
2025-04-16 12:55:08 +0100

    Implemented the buttons

--- src/griffith/MainMenu.java
+++ src/griffith/MainMenu.java
@@ -2,6 +2,7 @@ package griffith;
 
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.Rectangle;
 
@@ -12,13 +13,16 @@ public class MainMenu {
 	public Rectangle quitButton = new Rectangle(GamePanel.GAME_WIDTH / 2 + 120 ,350 ,100 ,50);
 	
 	public void render(Graphics g) {
-		
+		Graphics2D g2d = (Graphics2D) g;
 		
 		Font fnt0 = new Font("arial", Font.BOLD, 50);
 		g.setFont(fnt0);
 		g.setColor(Color.white);
 		g.drawString("Pong Pro Max", GamePanel.GAME_WIDTH / 3, 100);
 		
+		g2d.draw(playButton);
+		g2d.draw(helpButton);
+		g2d.draw(quitButton);
 	}
 	
 	
