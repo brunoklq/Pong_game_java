c510aac1dbf262ea1f50d921496c8b4dfdeeff78
Andrei <serianu.andrei@gmail.com>
2025-04-17 17:40:16 +0100

    Final Commit

--- src/griffith/MouseInput.java
+++ src/griffith/MouseInput.java
@@ -7,23 +7,7 @@ import java.awt.event.MouseListener;
 public class MouseInput implements MouseListener{
 
 	@Override
-	public void mouseClicked(MouseEvent e) {
-		int mx = e.getX();
-		int my = e.getY();
-		
-		//public Rectangle playButton = new Rectangle(GamePanel.GAME_WIDTH / 3 + 120 ,150 ,100 ,50);
-		//public Rectangle helpButton = new Rectangle(GamePanel.GAME_WIDTH / 3 + 120 ,250 ,100 ,50);
-	    //public Rectangle quitButton = new Rectangle(GamePanel.GAME_WIDTH / 3 + 120 ,350 ,100 ,50);
-		
-		//Play Button
-		if(mx >= GamePanel.GAME_WIDTH / 3 + 120 && mx <= GamePanel.GAME_WIDTH / 3 + 220) 
-		{
-			if(my >= 150 && my <= 200) 
-			{
-				GamePanel.State = GamePanel.STATE.GAME;
-			}
-		}
-	}
+	public void mouseClicked(MouseEvent e) {}
 
 	public void mousePressed(MouseEvent e) 
 	{
@@ -66,21 +50,12 @@ public class MouseInput implements MouseListener{
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void mouseReleased(MouseEvent e) {}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void mouseEntered(MouseEvent e) {}
 
 	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void mouseExited(MouseEvent e) {}
 
 }
